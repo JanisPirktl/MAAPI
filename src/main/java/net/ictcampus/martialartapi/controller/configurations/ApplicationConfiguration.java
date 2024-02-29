976d5f774235a6553589e6b208c093a5e6ebc545
Janis Pirktl <janis.pirktl@outlook.com>
2024-02-29 10:07:22 +0100

    Base code for configurations

--- src/main/java/net/ictcampus/martialartapi/controller/configurations/ApplicationConfiguration.java
+++ src/main/java/net/ictcampus/martialartapi/controller/configurations/ApplicationConfiguration.java
@@ -1,4 +1,15 @@
 package net.ictcampus.martialartapi.controller.configurations;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
