3162dbcb7360f32d28d3a5975c2a13015ae3e56e
Janis Pirktl <janis.pirktl@outlook.com>
2024-02-29 10:48:29 +0100

    Implemented User Details Service Implementation logic

--- src/main/java/net/ictcampus/martialartapi/controller/services/UserDetailsServiceImpl.java
+++ src/main/java/net/ictcampus/martialartapi/controller/services/UserDetailsServiceImpl.java
@@ -1,4 +1,28 @@
 package net.ictcampus.martialartapi.controller.services;
 
+import net.ictcampus.martialartapi.controller.repositories.UserRepository;
+import net.ictcampus.martialartapi.model.models.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+
 public class UserDetailsServiceImpl {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                emptyList());
+    }
 }
