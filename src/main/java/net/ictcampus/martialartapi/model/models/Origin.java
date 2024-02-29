252374354533363d38657116efa458ac486f69bb
neuep3 <neuep3@bfh.ch>
2024-02-29 10:06:56 +0100

    models done

--- src/main/java/net/ictcampus/martialartapi/model/models/Origin.java
+++ src/main/java/net/ictcampus/martialartapi/model/models/Origin.java
@@ -1,4 +1,22 @@
 package net.ictcampus.martialartapi.model.models;
 
 public class Origin {
+    private Integer id_Origin;
+    private String name;
+
+    public Integer getId_Origin() {
+        return id_Origin;
+    }
+
+    public void setId_Origin(Integer id_Origin) {
+        this.id_Origin = id_Origin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/net/ictcampus/martialartapi/model/models/User.java
+++ src/main/java/net/ictcampus/martialartapi/model/models/User.java
@@ -1,4 +1,49 @@
 package net.ictcampus.martialartapi.model.models;
 
 public class User {
+    private Integer id_User;
+    private String username;
+    private String name;
+    private String surname;
+    private Integer martialart_Id;
+
+    public Integer getId_User() {
+        return id_User;
+    }
+
+    public void setId_User(Integer id_User) {
+        this.id_User = id_User;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getMartialart_Id() {
+        return martialart_Id;
+    }
+
+    public void setMartialart_Id(Integer martialart_Id) {
+        this.martialart_Id = martialart_Id;
+    }
 }
