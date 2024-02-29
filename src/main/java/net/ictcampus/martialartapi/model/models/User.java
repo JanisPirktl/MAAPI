b2513689b2e287573f6760d1d1930c02c84fbdaf
Janis Pirktl <janis.pirktl@outlook.com>
2024-02-29 10:31:57 +0100

    Added Password to User

--- src/main/java/net/ictcampus/martialartapi/model/models/User.java
+++ src/main/java/net/ictcampus/martialartapi/model/models/User.java
@@ -1,11 +1,31 @@
 package net.ictcampus.martialartapi.model.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_User;
+
+    @NotNull(message = "name is required")
+    @NotBlank(message = "username can't be empty")
     private String username;
     private String name;
     private String surname;
     private Integer martialart_Id;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank
+    @Length(min = 6, max = 255)
+    private String password;
 
     public Integer getId_User() {
         return id_User;
@@ -45,5 +65,13 @@ public class User {
 
     public void setMartialart_Id(Integer martialart_Id) {
         this.martialart_Id = martialart_Id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
